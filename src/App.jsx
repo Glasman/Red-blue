0a3fe9fe425397b7a8ae37578fecb8523f374653
Mark Glasman <glasman.mark@gmail.com>
2023-11-21 11:43:48 -0500

    imported & created Link tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div id="container">
-        <div id="navbar">{/* navigation here */}</div>
-        <div id="main-section">{<Routes>
-          <Route path="/blue" element={<h1>Blue</h1>}/>
-          <Route path="/red" element={<h1>Red</h1>} />
-          </Routes>}</div>
+        <div id="navbar">
+          <Link to="/blue">Blue</Link>
+          <Link to="/red">Red</Link>
+        </div>
+        <div id="main-section">
+          {
+            <Routes>
+              <Route path="/blue" element={<h1>Blue</h1>} />
+              <Route path="/red" element={<h1>Red</h1>} />
+            </Routes>
+          }
+        </div>
       </div>
     </>
   );
